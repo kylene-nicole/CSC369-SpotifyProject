07bcfca65c3e3e27a858e58493b27618e5603c8a
kylene-phillips <62404695+kylene-phillips@users.noreply.github.com>
2021-02-27 17:20:31 -0800

    Latest work. Just this python file.

--- client.py
+++ client.py
@@ -2,20 +2,20 @@ import json
 
 import requests
 
-from track import Track
+#from track import Track
 from playlist import Playlist
 
 class SpotifyClient:
     """ Perform actions to communicate with Spotify API """
-    def __init__(self, authorization_token, spotify_id):
+    def __init__(self, api_token, spotify_id = ""):
         """
         :param authorization_token (str): Spotify API token
         :param user_id (int): Spotify user id
         """
-        self.authorization_token = authorization_token
+        self.api_token = api_token;
         self.spotify_id = spotify_id
 
-    def playlist_title_prompt():
+    def playlist_title_prompt(self):
         """
         Prompt the user for their playlist URI, we will extract the ID from that
         Example: spotify:playlist:6hzUZESozP9M7qKxJ8voKe
@@ -27,25 +27,34 @@ class SpotifyClient:
         print(" > Click Share \n > Click Copy Spotify URI \n *** *** ***")
 
         spotify_uri = input("Paste Spotify URI here: ")
-        spotify_id = spotify_uri[17:]
-        print(spotify_id) # temporary 
-        return spotify_id
+        self.spotify_id = spotify_uri[17:]
 
-    def get_playlist_tracks(self, limit = 10): #limit...
+    def get_playlist_tracks(self):
         """
         :param limit (int): number of tracks to get... should be less than X
         :param playlist_id (str): the Spotify Playlist ID extracted from user URI
         :return tracks (list of tracks): List of the playlist tracks
         """
-        spotify_id = self.playlist_title_prompt()
-        print(spotify_id)
+        print(self.spotify_id)
+        
         #with or without requests.get.... ?
-        url = f"https://api.spotify.com/v1/playlists/{spotify_id}"
-        response = self._place_get_api_request(url)
+        url = f"https://api.spotify.com/v1/playlists/{self.spotify_id}"
+        response = requests.get(
+            url,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self.api_token}"
+            }
+        )
 
         response_json = response.json()
         print(response_json)
-        #tracks = # Rachel how do i use json to access tracks and save them? See:
+        
+        print('**************')
+        
+        results = response_json['tracks']['items']
+        
+        print(results)
        
         """
         https://developer.spotify.com/documentation/web-api/reference/#object-playlistobject
@@ -58,7 +67,7 @@ class SpotifyClient:
     https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-recommendations
     https://developer.spotify.com/documentation/web-api/reference/#category-browse
     """
-    def get_recommendations(self, seed_tracks, limit = 50):
+    def get_recommendations(self, seed_tracks):
         """
         """
         seed_tracks_url = ""
@@ -66,7 +75,7 @@ class SpotifyClient:
             seed_tracks_url += seed_tracks.id + ","
         seed_tracks_url = seed_tracks_url[:-1]
         url = f""
-        response = self._place_get_api_request(url)
+        response = requests.get(url)
         response_json = response.json()
         #tracks = ...
         #return tracks
@@ -77,29 +86,17 @@ class SpotifyClient:
         #data = json.dumps()
         #print
 
-
-class Track:
-    """  """
-    def __init__(self, name, id, artist):
-        """
-        :param name (str): Track name
-        :param id (int): Spotify track id
-        :param artist (str): Artist who created the track 
-        """
-        self.name = name
-        self.id = id
-        self.artist = artist
-
-    # i dont think we need this since we are just outputtng a song not playlist
-    def create_spotify_uri(self):
-        return f"spotify:track:{self.id}"
-
-    def __str__(self):
-        return f"{self.name} by {self.artist}"
+def main():
+    # Normally would use an OS to store this but cant if we are working together...
+    SPOTIFY_AUTH_TOKEN = 'BQDbkW7L3YCgPtyJ3mq048rNzSprTNFAz25oYVgoXGO1bmizUHujpulYnS4L5o0dPl8H_wVp97iruUBbdifX3ioni9N9Nr442_1iCP9ykQpdLo08XkfEKs8QiHFzwWMshRp9LKqK2lC07a_4UdrJq40iiJlCghQ'
+    sc = SpotifyClient(SPOTIFY_AUTH_TOKEN, "")
+    
+    sc.playlist_title_prompt()
+    sc.get_playlist_tracks()
+    
+    #get recommendations...\
+    #output song.
+    #WEB API?
 
 if __name__ == '__main__':
-    SpotifyClient.playlist_title_prompt()
-
-
-
-
+    main()
