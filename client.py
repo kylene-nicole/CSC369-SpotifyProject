daa049b3aa5829ebf50b22c9879ac3a35a999bdb
jonathanshan <johnny.shan@gmail.com>
2021-03-17 23:19:30 -0700

    recommender done

--- client.py
+++ client.py
@@ -2,7 +2,6 @@ import json
 
 import requests
 
-#from track import Track
 from playlist import Playlist
 
 class SpotifyClient:
@@ -15,6 +14,7 @@ class SpotifyClient:
         self.api_token = api_token
         self.spotify_id = spotify_id
         self.response_json = None
+        self.song_recs = None
 
     def playlist_title_prompt(self):
         """
@@ -30,50 +30,7 @@ class SpotifyClient:
         spotify_uri = input("Paste Spotify URI here: ")
         self.spotify_id = spotify_uri[17:]
 
-    def get_playlist_tracks(self, offset):
-        """
-        :param limit (int): number of tracks to get... should be less than X
-        :param playlist_id (str): the Spotify Playlist ID extracted from user URI
-        :return tracks (list of tracks): List of the playlist tracks
-        """
-        print(self.spotify_id)
         
-        #with or without requests.get.... ?
-        url = f"https://api.spotify.com/v1/playlists/{self.spotify_id}/tracks"
-        response = requests.get(
-            url,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {self.api_token}"
-            },
-            params={
-                "offset": offset,
-                "limit": 100
-            }
-        )
-            
-
-        self.response_json = response.json()
-        print(self.response_json)
-        
-        print('**************')
-        
-        
-        #results = response_json['tracks']['items']
-        
-        #print(results)
-       
-        """
-        https://developer.spotify.com/documentation/web-api/reference/#object-playlistobject
-        """
-        #return tracks
-
-    # checking out spotify api endpoint reference....  BROWSE ~ they have
-    # all the metrics already listed...
-    """
-    https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-recommendations
-    https://developer.spotify.com/documentation/web-api/reference/#category-browse
-    """
     
     def get_playlist_tracks1(self, offset):
         """
@@ -110,18 +67,64 @@ class SpotifyClient:
 
         return list_json
     
-    def get_recommendations(self, seed_tracks):
+    def get_recommendations(self, file):
         """
         """
-        seed_tracks_url = ""
-        for tracks in seed_tracks:
-            seed_tracks_url += seed_tracks.id + ","
-        seed_tracks_url = seed_tracks_url[:-1]
-        url = f""
-        response = requests.get(url)
-        response_json = response.json()
-        #tracks = ...
-        #return tracks
+        spark_df = spark.read.csv(file, header = True, quote = "\"", escape = "\"")
+        
+        # Make sure rating is numeric
+        spark_df = spark_df.withColumn('rating_numeric', spark_df['rating'].cast('double'))
+        
+        # Generate numeric IDs from song_id
+        song_indexer = StringIndexer(inputCol='song_id', outputCol='song_id_numeric')
+        temp = song_indexer.fit(spark_df).transform(spark_df)
+        
+        # Generate numeric IDs from playlist_id
+        pl_indexer = StringIndexer(inputCol='playlist_id', outputCol='playlist_id_numeric')
+        final_df = pl_indexer.fit(temp).transform(temp)
+        
+        (training, test) = final_df.randomSplit([0.8, 0.2])
+        # The ALS function only takes numeric values from ids
+        als = ALS(maxIter=10, regParam=0.1, userCol="playlist_id_numeric", itemCol="song_id_numeric", 
+                  ratingCol="rating_numeric", coldStartStrategy="drop", implicitPrefs=False)
+        model = als.fit(training)
+        
+        # build and evaluate model
+        predictions = model.transform(test)
+        evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating_numeric",
+                                    predictionCol="prediction")
+        rmse = evaluator.evaluate(predictions)
+        
+        # Recommendations are formatted playlist_id_numeric : [song_id_numeric]
+        # Need to get the actual song names instead of IDs
+        playlist_df = final_df.select(final_df["playlist_id"],final_df["playlist_id_numeric"]).drop_duplicates()
+        song_df = final_df.select(final_df["song_id_numeric"], final_df["song"], final_df["artist"]).drop_duplicates()
+        
+        # Generate top 200 song recs for every playlist
+        plRecs = model.recommendForAllUsers(200)
+        
+        # find recs for a given playlist
+        recs = plRecs.join(playlist_df, plRecs.playlist_id_numeric == playlist_df.playlist_id_numeric)\
+                .filter(playlist_df.playlist_id  == self.spotify_id)\
+                .select(plRecs.recommendations)
+        
+        # The ALS function does not prevent songs already in the playlist from being recommended
+        # We must manually remove all songs that are already in the playlist
+        recommends = spark.createDataFrame(recs.head()[0])
+        already_in = final_df.filter(final_df.playlist_id  == self.spotify_id)
+        filtered_recs = recommends.join(already_in, on=['song_id_numeric'], how='left_anti')
+        
+        # convert recs from dataframe to rdd and find their song names by joining with song_df
+        # Grab top 25 suggestions and output them
+        rec_rdd = filtered_recs.rdd.map(lambda line: tuple([x for x in line]))
+        song_names = song_df.rdd.map(lambda line: (line[0], (line[1], line[2]))).join(rec_rdd)\
+                            .sortBy(lambda z: z[1][1]).take(25)
+        
+        print("Here are some song suggestions for your playlist:")
+        [print(x[1][0][0] + " by " + x[1][0][1]) for x in song_names]
+
+        self.song_recs = song_names
+        
 
     def output_songs(self):
         """
@@ -131,21 +134,16 @@ class SpotifyClient:
 
 def main():
     # Normally would use an OS to store this but cant if we are working together...
-    SPOTIFY_AUTH_TOKEN = 'BQAPzXQWJ_YdqlHrR6hX8JBv6cMvM2-kIrppEcDaJD3vMOHFoLx7KM44EeD9aWswFJU0-9IcYunHWk_9xOY'
-    sc = SpotifyClient(SPOTIFY_AUTH_TOKEN, "")
-    
-    sc.playlist_title_prompt()
-    sc.get_playlist_tracks(200)
-    #sc.response_json
-    
-    
-    
-    #get recommendations...\
-    #output song.
-    #WEB API?
+    spotC = SpotifyClient(SPOTIFY_AUTH_TOKEN, "")
+    spotC.playlist_title_prompt()
+    all_songs = spotC.get_playlist_tracks1(0)
+    update_dask("data.txt", all_songs, spotC)
 
+    spotC.get_recommendations('data.txt')
 if __name__ == '__main__':
     main()
+
+# # Imports and API Access 
 
 # +
 import pandas as pd
@@ -155,6 +153,13 @@ import dask.array as da
 from pyspark.sql import SparkSession
 import requests
 import time
+import os
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
+from pyspark.sql import Row
+from pyspark.sql.functions import col, row_number
+from pyspark.sql.window import Window
+from pyspark.ml.feature import StringIndexer
 
 spark = SparkSession \
     .builder \
@@ -186,52 +191,36 @@ auth_url = 'https://accounts.spotify.com/api/token'
 SPOTIFY_AUTH_TOKEN = get_token(client_id, client_secret, auth_url)
 SPOTIFY_AUTH_TOKEN
 
-# ----------------------------
+# # Create initial data file
 
 
-# for rachel!
-# Normally would use an OS to store this but cant if we are working together...SPOTIFY_AUTH_TOKEN = 'BQBdDRxg2FxcZ2AoSp9DUJQTi5NcX1uRWnwLmst0nHypW0-YMw_WGvSm00j4AHMTGZf-Z6VcE51PqXeLb0vYpSZ8zpDDCbKzq4_q94KVac1wlGk3_egC8cydo5LXEyEHvCXOXjEpzjIt2Pb7UuWrRH-MDZk-sGA'
-spotC = SpotifyClient(SPOTIFY_AUTH_TOKEN, "")
-
-# Run script 
-spotC.playlist_title_prompt()
-spotC.get_playlist_tracks(0)
-playlist_json = spotC.response_json
-spotC.response_json
-
-# Playlists with >100 songs
-spotC.playlist_title_prompt()
-all_songs = spotC.get_playlist_tracks1(0)
-
-# +
-# create initial data set (don't use after first run)
-first = pd.DataFrame()
-l_a = []
-for i in playlist_json['items']:
-    song = i['track']
-    l_a.append([song['artists'][0]['name'], song['name'], song['id'],  spotC.spotify_id, 1])
-    
-temp = pd.DataFrame(l_a)
-first = first.append(temp, ignore_index = True)
-first.columns = ['artist', 'song', 'song_id', 'playlist_id', 'rating']
-first.to_csv(r'data.txt', index=None, sep=',')
-first
+# create inital data file
+def create_dataset(file):
+    if os.path.isfile(file):
+        print ("File already exists")
+        return
+    else:
+        f = open(file,"w+")
+        f.write("artist,song,song_id,playlist_id,rating\n")
+        f.close()
+        return
 
 
-# -
+# # PANDAS update dataset
 
-# PANDAS
-# update dataset
 def update_pandas(file, json_datas, spotify_client):
-    initial_pd = pd.read_csv(file)
+    initial_pd = pd.read_csv(file, header = 0)
+    initial_pd
     list_add = []
     for json_data in json_datas:
         for i in json_data['items']:
             song = i['track']
-            if song['id'] is not None:
-                list_add.append([song['artists'][0]['name'], song['name'], song['id'],  spotify_client.spotify_id, 1])
+            if song is not None:
+                if song['id'] is not None:
+                    list_add.append([song['artists'][0]['name'], song['name'], song['id'],  spotify_client.spotify_id, 1])
     new_pd = pd.DataFrame(list_add, columns = ['artist', 'song', 'song_id',  'playlist_id', 'rating'])
-    new_pd.astype({'rating' : initial_pd['rating'].dtype.name})
+    if len(initial_pd) > 0:
+        new_pd.astype({'rating' : initial_pd['rating'].dtype.name})
     final_pd = pd.concat([initial_pd, new_pd], ignore_index = True).drop_duplicates().reset_index(drop=True)
     existing_rows = initial_pd.shape[0]
     new_rows = final_pd.shape[0]
@@ -241,13 +230,8 @@ def update_pandas(file, json_datas, spotify_client):
     return "No new songs to add"
 
 
-start = time.time()
-update_pandas("data.txt", all_songs, spotC)
-end = time.time()
-print(end - start)
+# # DASK update dataset
 
-
-# DASK update dataset
 def update_dask(file, json_datas, spotify_client, chunk = 200):
     start = time.time()
     initial_dd = dd.read_csv(file)
@@ -255,10 +239,11 @@ def update_dask(file, json_datas, spotify_client, chunk = 200):
     for json_data in json_datas:
         for i in json_data['items']:
             song = i['track']
-            if song['id'] is not None:
-                list_dd.append([song['artists'][0]['name'], song['name'], song['id'],  spotify_client.spotify_id, 1])
+            if song is not None:
+                if song['id'] is not None:
+                    list_dd.append([song['artists'][0]['name'], song['name'], song['id'],  spotify_client.spotify_id, 1])
     end = time.time()
-    print(end - start)
+#     print(end - start)
 
     new_pd = pd.DataFrame(list_dd, columns = ['artist', 'song', 'song_id', 'playlist_id', 'rating'])
     new_pd.astype({'rating' : 'int64'})
@@ -266,7 +251,7 @@ def update_dask(file, json_datas, spotify_client, chunk = 200):
     final_dd = dd.concat([initial_dd, new_dd], axis=0,interleave_partitions=True).drop_duplicates().reset_index(drop=True)
     
     end = time.time()
-    print(end - start)
+#     print(end - start)
 
     existing_rows = initial_dd.shape[0]
     new_rows = final_dd.shape[0]
@@ -274,23 +259,21 @@ def update_dask(file, json_datas, spotify_client, chunk = 200):
         final_dd.loc[existing_rows:].to_csv('data.txt', index=None, sep=',',mode='a', header = False)
         
         end = time.time()
-        print(end - start)
+#         print(end - start)
         return final_dd
     return "No new songs to add"
 
-# start = time.time()
-update_dask("data.txt", all_songs, spotC)
-# end = time.time()
-# print(end - start)
+# # SPARK update dataset
 
-# SPARK update dataset
 # notes: method cannot specify saved file name
 def update_spark(file, json_data, spotify_client):
     initial_spark = spark.read.csv(file, header = True, quote = "\"", escape = "\"")
     list_spark = []
     for i in json_data['items']:
         song = i['track']
-        list_spark.append([song['artists'][0]['name'], song['name'], song['id'],  spotify_client.spotify_id, 1])
+        if song is not None:
+                if song['id'] is not None:
+                    list_spark.append([song['artists'][0]['name'], song['name'], song['id'],  spotify_client.spotify_id, 1])
 
     spark_rdd = sc.parallelize(list_spark)
     new_spark = spark_rdd.toDF(['artist', 'song', 'song_id', 'playlist_id', 'rating'])
@@ -305,24 +288,15 @@ def update_spark(file, json_data, spotify_client):
 
 update_spark("data.txt", playlist_json, spotC)
 
-# get recommendations...\
-# output song.
-# WEB API?
-
-# notes
-['tracks']['items']
-spotify:playlist:1GN4sQhsGhAOfxIaURcC9c
-        
-        songid
-        artist
-        name
-        artist id
-        popular id
-        > ALS jonathan
+# # Generate Predictions
 
 # +
-# 1. Saving and writing into csv or txt file. Optimally this would be implemented with distributed computing
-# 2. (Optional) Implement multi-hundreds
-# 3. Find public URLs and add them to our data set
-# 4. Implement PySpark ALS fucntion
-# 5. (Optional) Print images
+spotC = SpotifyClient(get_token(client_id, client_secret, auth_url), "")
+spotC.playlist_title_prompt()
+all_songs = spotC.get_playlist_tracks1(0)
+update_dask("data.txt", all_songs, spotC)
+
+spotC.get_recommendations('data.txt')
+# -
+
+
--- recommend.py
+++ recommend.py
@@ -1,4 +1,95 @@
-"""
-Visit source: 
-https://www.youtube.com/watch?v=3vvvjdmBoyc&t=3s
-"""
+# # This file is to test the Evaluator
+# # no implemention in this file
+
+# +
+from pyspark.sql import SparkSession
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
+from pyspark.sql import Row
+from pyspark.sql.functions import col, row_number
+from pyspark.sql.window import Window
+from pyspark.ml.feature import StringIndexer
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark") \
+    .getOrCreate()
+
+sc = spark.sparkContext
+# -
+
+spark_df = spark.read.csv('data.txt', header = True, quote = "\"", escape = "\"")
+
+spark_df = spark_df.withColumn('rating_numeric', spark_df['rating'].cast('double'))
+
+# Generate numeric IDs from song_id
+song_indexer = StringIndexer(inputCol='song_id', outputCol='song_id_numeric')
+temp = song_indexer.fit(spark_df).transform(spark_df)
+
+# Generate numeric IDs from playlist_id
+pl_indexer = StringIndexer(inputCol='playlist_id', outputCol='playlist_id_numeric')
+final_df = pl_indexer.fit(temp).transform(temp)
+
+(training, test) = final_df.randomSplit([0.9, 0.1])
+
+# The ALS function only takes numeric values from ids
+als = ALS(maxIter=10, regParam=0.01, userCol="playlist_id_numeric", itemCol="song_id_numeric", ratingCol="rating_numeric",
+          coldStartStrategy="drop", implicitPrefs=False)
+model = als.fit(training)
+
+predictions = model.transform(test)
+evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating_numeric",
+                                predictionCol="prediction")
+rmse = evaluator.evaluate(predictions)
+print("Root-mean-square error = " + str(rmse))
+
+
+# Generate top 10 song recs for every playlist
+plRecs = model.recommendForAllUsers(100)
+# Generate top 10 playlist recs for each song
+songRecs = model.recommendForAllItems(10)
+
+final_df.select(final_df["playlist_id"],final_df["playlist_id_numeric"]).filter(final_df.playlist_id  == "4LtjJ8L0V3Vc0blNiBe7s7").drop_duplicates().collect()
+
+playlist_df = final_df.select(final_df["playlist_id"],final_df["playlist_id_numeric"]).drop_duplicates()
+song_df = final_df.select(final_df["song_id_numeric"], final_df["song"], final_df["artist"]).drop_duplicates()
+
+plRecs.show()
+
+a = plRecs.join(playlist_df, plRecs.playlist_id_numeric == playlist_df.playlist_id_numeric)\
+    .filter(playlist_df.playlist_id  == "4LtjJ8L0V3Vc0blNiBe7s7")\
+    .select(plRecs.recommendations)
+
+
+c = spark.createDataFrame(a.head()[0])
+
+b = final_df.filter(final_df.playlist_id  == "4LtjJ8L0V3Vc0blNiBe7s7")
+
+c.join(b, on=['song_id_numeric'], how='left_anti').show()
+
+final_df.filter(final_df.song_id_numeric == 6).show()
+
+b.filter(b.song_id_numeric == 7).show()
+
+song_df.filter(song_df.song_id_numeric == 53).show()
+
+df.as("table1").join(
+  df2.as("table2"),
+  $"table1.name" === $"table2.name" && $"table1.age" === $"table2.howold",
+  "leftanti"
+)
+
+song_df.rdd.map(lambda line: tuple([x for x in line])).collect()
+
+
+
+songs = song_df.rdd.map(lambda line: (line[0], (line[1], line[2]))).join(rdd).sortBy(lambda z: z[1]).collect()
+songs
+
+rdd = sc.parallelize(a.head()[0]).map(lambda line: tuple([x for x in line]))
+rdd.collect()
+
+print_songs = [x[1][0][0] + ": " + x[1][0][1] for x in songs]
+print_songs
+
+song_df.filter(song_df.artist == "Giveon").show()
